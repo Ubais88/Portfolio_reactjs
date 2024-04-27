e937025722b0485627d93cfb7405d11586f1f7c5
Ubais88 <moubais2143@gmail.com>
2024-04-27 08:59:24 +0530

    skills and exprience section added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
 import styled from "styled-components";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   overflow-x: hidden;
 `;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+
 function App() {
 
   return (
@@ -19,6 +38,10 @@ function App() {
         <Navbar />
         <Body>
           <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
         </Body>
       </Router>
     </ThemeProvider>
